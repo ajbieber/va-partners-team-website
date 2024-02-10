33125556ff3beb1dd12f5b54e2dad36692f75543
Austin Bieber <ajbieber1995@gmail.com>
2024-02-10 12:54:39 -0700

    Added pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import TeamMember from './components/TeamMember/TeamMember';
 import blank from './images/blank.png';
 import abieber from './images/abieber.jpeg';
+import { useState } from 'react';
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("description");
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,18 +14,55 @@ function App() {
       </header>
       <div>
         <ul>
-          <li>Description</li>
-          <li>Team Members</li>
-          <li>Goals</li>
-          <li>Lighthouse Scores</li>
+          <li 
+            onClick={() => setSelectedTab("description")}
+            className={(selectedTab === "description" ? 'li-selected' : 'li-not-selected')}
+          >Description</li>
+          <li
+            onClick={() => setSelectedTab("team")}
+            className={(selectedTab === "team" ? 'li-selected' : 'li-not-selected')}
+          >Team Members</li>
+          <li
+            onClick={() => setSelectedTab("goals")}
+            className={(selectedTab === "goals" ? 'li-selected' : 'li-not-selected')}
+          >Goals</li>
+          <li
+            onClick={() => setSelectedTab("lighthouse")}
+            className={(selectedTab === "lighthouse" ? 'li-selected' : 'li-not-selected')}
+          >Lighthouse Scores</li>
         </ul>
 
         <div id="body">
-          <div id="team-members-page">
+          <div id="desc" className={(selectedTab === "description" ? 'selected' : 'not-selected')}>
+            <p>
+              The VA Partners team is dedicated to improving the partners application for the Vision
+              Aid organization. This application is currently being used in production by the Vision
+              Aid organization and its partners in hospitals around India.
+            </p>
+            <p>
+              The application we will be improving allows for hospitals and partner organizations to track disbursement of
+              devices used to by the visually impaired, in a variety of situations.
+            </p>            
+          </div>
+          <div className={(selectedTab === "team" ? 'selected' : 'not-selected')}>
             <TeamMember imagePath={blank} name="Amber Molina" role="Team Lead" />
             <TeamMember imagePath={abieber} name="Austin Bieber" role="Tech Lead" />
-            <TeamMember imagePath={blank} name="Chrisin Lin" role="Frontend" />
+            <TeamMember imagePath={blank} name="Christin Lin" role="Frontend" />
             <TeamMember imagePath={blank} name="Nasa Quba" role="Backend" />
+          </div>
+          <div id="goals" className={(selectedTab === "goals" ? 'selected' : 'not-selected')}>
+            <p>Our goals for this semeter are as follows:</p>
+            <p></p>
+            <p></p>
+            <ol>
+              <li>Rewrite authentication system to be more seamless</li>
+              <li>Integrate authorization into the authentication system</li>
+              <li>Design a customizable landing page that doesn't require dev assistance</li>
+              <li>Upgrade the UI to meet accessability standards</li>
+              <li>Implement a feedback form for users to report issues and bugs</li>
+              <li>Add a footer to all pages</li>
+              <li>Implement any new changes specified by stakeholders</li>
+            </ol>
           </div>
         </div>
       </div>
