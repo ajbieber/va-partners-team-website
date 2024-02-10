f8c703f73f78cc799a1fee775ee4f28f2f1a3698
Austin Bieber <ajbieber1995@gmail.com>
2024-02-10 12:12:48 -0700

    Major refresh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import TeamMember from './components/TeamMember/TeamMember';
+import blank from './images/blank.png';
+import abieber from './images/abieber.jpeg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Vision Aid Partners - Spring 2024
       </header>
+      <div>
+        <ul>
+          <li>Description</li>
+          <li>Team Members</li>
+          <li>Goals</li>
+          <li>Lighthouse Scores</li>
+        </ul>
+
+        <div id="body">
+          <div id="team-members-page">
+            <TeamMember imagePath={blank} name="Amber Molina" role="Team Lead" />
+            <TeamMember imagePath={abieber} name="Austin Bieber" role="Tech Lead" />
+            <TeamMember imagePath={blank} name="Chrisin Lin" role="Frontend" />
+            <TeamMember imagePath={blank} name="Nasa Quba" role="Backend" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
