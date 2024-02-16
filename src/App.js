87f4b413d2db5422c9770008a5eae8b872c7df4e
Austin Bieber <ajbieber1995@gmail.com>
2024-02-16 12:13:28 -0700

    Removed video code

--- src/App.js
+++ src/App.js
@@ -72,22 +72,13 @@ function App() {
             </ol>
           </div>
           <div id="lighthouse" className={(selectedTab === "lighthouse" ? 'selected' : 'not-selected')}>
-          <ol>
+            <ol>
               <li>Performance: 100</li>
               <li>Accessability: 100</li>
               <li>Best Practices: 100</li>
               <li>SEO: 100</li>
             </ol>
           </div>
-
-<div style="padding:56.25% 0 0 0;position:relative;">
-<iframe src="https://player.vimeo.com/video/913052661?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="VA-Partners_MiniPresentation">
-</iframe>
-</div>
-<script src="https://player.vimeo.com/api/player.js">
-</script>
-
-
         </div>
       </div>
     </div>
